46faf1c3a97a3e719cb18f2642afd65e4cc469af
matte <matteo.chambrellan@gmail.com>
2024-02-13 13:43:43 +0100

    tout

---
+++ dashfront/src/app/compo-test/compo-test.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-compo-test',
+  standalone: true,
+  imports: [],
+  templateUrl: './compo-test.component.html',
+  styleUrl: './compo-test.component.scss'
+})
+export class CompoTestComponent {
+
+}

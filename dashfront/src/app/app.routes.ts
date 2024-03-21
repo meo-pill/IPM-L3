d1433616c571b661d1484969e31d619a53ee3408
meo <83820503+meo-pill@users.noreply.github.com>
2024-03-21 17:08:08 +0100

    Update app.routes.ts

--- dashfront/src/app/app.routes.ts
+++ dashfront/src/app/app.routes.ts
@@ -7,10 +7,9 @@ import { FooterComponent } from "./components/footer/footer.component";
 export const routes: Routes = [
     // Define your routes here
     // { path: '', component: YourComponent },
-    { path: 'header', component: HeaderComponent }
     { path: 'header', component: HeaderComponent },
     { path: 'select', component: SelectSatellitesComponent },
-    { path: 'map', component: GooglemapsComponent }
+    { path: 'map', component: GooglemapsComponent },
     { path: 'footer', component: FooterComponent}
 ];
 

cb7eec7efc3703cb7c3c1c4fb8a4cd8897c62df0
matte <matteo.chambrellan@gmail.com>
2024-03-22 11:44:15 +0100

    Récupération desc, Gestion des erreurs & fixes

--- dashfront/src/app/app.module.ts
+++ dashfront/src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-
 import { routes } from './app.routes';
-import { AppComponent } from "./app.component";
-import { HeaderComponent } from "./components/header/header.component";
 
+import { ApiService } from "./services/api.service";
+import { HeaderComponent } from "./components/header/header.component";
 import { HttpClientModule } from "@angular/common/http";
 import { RouterModule } from "@angular/router";
-import { ApiService } from "./services/api.service";
 import { GooglemapsComponent } from "./components/googlemaps/googlemaps.component";
 import { GoogleMapsModule } from "@angular/google-maps";
 import { SelectSatellitesComponent } from "./components/select-satellites/select-satellites.component";
@@ -18,7 +16,15 @@ import { FooterComponent } from "./components/footer/footer.component";
     declarations: [
     ],
     imports: [
-    ],
+      BrowserModule,
+      routes,
+      HttpClientModule,
+      RouterModule,
+      GoogleMapsModule,
+      GooglemapsComponent,
+      HeaderComponent,
+      FooterComponent,
+      SelectSatellitesComponent],
     providers: [ApiService],
 })
 export class AppModule { }
